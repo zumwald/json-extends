b657e60b7ccbedf70efde123e57ee672c16a26a5
iansul <iansul@live.com>
2020-04-09 21:18:57 -0700

    Added support for custom merge function to GetJsonObject (#3)
    Swtiched to full lodash package instead of multiple targeted pacakges.
    
    Co-authored-by: NORTHAMERICA\iansul <iansul@microsoft.com>

--- src/index.test.ts
+++ src/index.test.ts
@@ -43,3 +43,28 @@ test("Json file with multiple extends", () => {
   expect(result).toHaveProperty("color", "none");
   expect(Object.getOwnPropertyNames(result)).toHaveLength(5);
 });
+
+test("Json file with json5 syntax and custom merge functon that concats array with null initial element", () => {
+  function CustomMerge(objValue: object, srcValue: object): object {
+    // If we are merging two arrays
+    if (Array.isArray(objValue) && Array.isArray(srcValue)) {
+      let srcValueArray = <Array<object>>srcValue;
+      // If object we are merging into (srcValue) has an explicit null value as it's first element
+      if (srcValueArray.length > 0 && srcValueArray[0] == null) {
+        // Strip the null out and concat arrays
+        return (<Array<object>>objValue).concat(srcValueArray.slice(1));
+      }
+    }
+  }
+  let result = GetJsonObject(testDir + "config-5.json5", 25, CustomMerge);
+  expect(result).toHaveProperty("foo", "yellow");
+  expect(result).toHaveProperty("bar");
+  expect(result.bar).toHaveLength(4);
+  expect(result.bar[0]).toHaveProperty("a", true);
+  expect(result.bar[1]).toHaveProperty("a", false);
+  expect(result.bar[1]).toHaveProperty("b", "apple");
+  expect(result.bar[2]).toHaveProperty("a", "overridden!");
+  expect(result.bar[3]).toHaveProperty("a", "overridden again!");
+  expect(result).toHaveProperty("extends");
+  expect(Object.getOwnPropertyNames(result)).toHaveLength(3);
+});
